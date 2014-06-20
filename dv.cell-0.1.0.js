c5037da3e9f0579bd0bfc5c5c0a18f281227f3f7
Rodsevich <neton_12@hotmail.com>
2014-06-20 13:03:45 -0300

    Refactoring chiquito + supresión de warnings de JS

--- dv.cell-0.1.0.js
+++ dv.cell-0.1.0.js
@@ -10,8 +10,8 @@ var dvCell = function(id, x, y, map)
 	this.draw = function()
 	{
 		map.context.fillRect(this.x + 1, this.y + 1, this.side - 2, this.side - 2);
-	}
-}
+	};
+};
 
 var dvMap = function()
 {
@@ -19,21 +19,21 @@ var dvMap = function()
 	this.cellHeight = 10;
 	this.cellSide = 25;
 	this.obstacles = null;
-        this.intervalo_dibujo_path = 200;
         this.startCell = null;
 	this.endCell = null;	
-	
-	this.canvas = jQuery('<canvas width="' + this.width() + 'px" height="' + this.height() + 'px"></canvas>');
+        this.intervalo_dibujo_path = 200;
+        
+        this.canvas = jQuery('<canvas width="' + this.cellWidth * this.cellSide + 'px" height="' + this.cellHeight * this.cellSide + 'px"></canvas>');
 	this.context = this.canvas[0].getContext('2d');
         
 	this.isObstacle = function(id)
 	{
 	  return this.getObstacles()[id];
-	}
+	};
 	
 	this.getObstacles = function()
 	{	
-	  if (this.obstacles == null)
+	  if (this.obstacles === null)
 	  {
 	    this.obstacles = new Array();
 	    
@@ -44,7 +44,7 @@ var dvMap = function()
 	  }
 	  
 	  return this.obstacles;
-	}
+	};
 	
 	this.addObstacle = function(aCell)
 	{
@@ -53,51 +53,51 @@ var dvMap = function()
 	  this.obstacleMode();
 	  
 	  aCell.draw();
-	}
+	};
 	
 	this.countCells = function()
 	{
 		return this.cellWidth * this.cellHeight;
-	}
+	};
 	
 	this.width = function()
 	{
 		return this.cellWidth * this.cellSide;
-	}
+	};
 	
 	this.height = function()
 	{
 		return this.cellHeight * this.cellSide;
-	}
+	};
 	
 	this.drawMode = function()
 	{		
 		this.context.fillStyle = '#dddddd';
-	}
+	};
 	
 	this.selectStartCellMode = function()
 	{	
 		this.context.fillStyle = '#88ee77';
-	}
+	};
 	
 	this.selectEndCellMode = function()
 	{	
 		this.context.fillStyle = '#8877ee';
-	}
+	};
 	
 	this.resolveMode = function()
 	{	
 		this.context.fillStyle = '#77ddee';
-	}
+	};
 	
 	this.obstacleMode = function()
 	{
 		this.context.fillStyle = '#ee7777';
-	}
+	};
 	
 	this.resolve = function()
 	{
-		if ((this.startCell == null) || (this.endCell == null))
+		if ((this.startCell === null) || (this.endCell === null))
 		{
 			return false;
 		}
@@ -105,7 +105,7 @@ var dvMap = function()
 		this.resolveMode();
 		queuedBy = this.buildPath(this.startCell, this.endCell);
 	  
-	  if (queuedBy[this.endCell.id] == -1)
+	  if (queuedBy[this.endCell.id] === -1)
 	  {	
 		  alert('No path!');
 	  }
@@ -115,7 +115,7 @@ var dvMap = function()
 	    
 	    cell = queuedBy[this.endCell.id];
 	    
-	    while (cell.id != this.startCell.id)
+	    while (cell.id !== this.startCell.id)
 	    {
 	      path.push(cell);
 	      
@@ -141,56 +141,55 @@ var dvMap = function()
 	    }, this.intervalo_dibujo_path); 		  
 		return path;
 	  }
-	}
+	};
 	
-        this.buildPath = function(startCell, endCell)
-        {
-            queue = new Array();
-            visited = new Array();
-            queuedBy = new Array();
+	this.buildPath = function(startCell, endCell)
+	{
+	  queue = new Array();
+	  visited = new Array();
+	  queuedBy = new Array();
+	  
+	  for (i = 0; i < this.countCells(); i++)
+	  {	
+	    visited.push(false);
+	    queuedBy.push(-1);
+	  }
+	  
+	  queue.push(startCell);
+	  
+	  while (queue.length > 0)
+	  {	    	    
+	    cell = queue.shift();
+	    
+	    if (cell.id === endCell.id)
+	    {
+		  //console.log(queuedBy);
+	      return queuedBy;
+	    }
+	    
+      neighbours = this.getNeighbours(cell);
+            
+      for (i = 0; i < neighbours.length; i++)
+	    {	      
+	      neighbour = neighbours[i];
+	      
+	      if (!visited[neighbour.id] && !this.isObstacle(neighbour.id))
+	      {	    	      
+    	    visited[neighbour.id] = true;
+    	    
+    	    queuedBy[neighbour.id] = cell;
+    	    
+	        queue.push(neighbour);
 
-            for (i = 0; i < this.countCells(); i++)
-            {
-                visited.push(false);
-                queuedBy.push(-1);
-            }
-
-            queue.push(startCell);
-
-            while (queue.length > 0)
-            {
-                cell = queue.shift();
-
-                if (cell.id == endCell.id)
-                {
-                    //console.log(queuedBy);
-                    return queuedBy;
-                    ;
-                }
-
-                neighbours = this.getNeighbours(cell);
-
-                for (i = 0; i < neighbours.length; i++)
-                {
-                    neighbour = neighbours[i];
-
-                    if (!visited[neighbour.id] && !this.isObstacle(neighbour.id))
-                    {
-                        visited[neighbour.id] = true;
-
-                        queuedBy[neighbour.id] = cell;
-
-                        queue.push(neighbour);
-
-                    }
-                }
-            }
-
-            return queuedBy;
-        }
-
-        this.getNeighbours = function(aCell)
-        {
+	      }	    	        
+	    }	    
+	  }
+	  
+	  return queuedBy;
+	};
+	
+	this.getNeighbours = function(aCell)
+	{
 	  aux = new Array();
 	  
 	  // Top-left
@@ -221,21 +220,21 @@ var dvMap = function()
 	  
 	  for (i = 0; i < aux.length; i++)
 	  {
-	    if (aux[i] != null)
+	    if (aux[i] !== null)
 	    {
 	      neighbours.push(aux[i]);
 	    }
 	  }
 
 	  return neighbours;
-	}
+	};
 	
 	this.unselect = function(aCell)
 	{
 		this.drawMode();
 		
 		aCell.draw();
-	}
+	};
 	
 	this.getCellById = function(id)
 	{		
@@ -244,7 +243,7 @@ var dvMap = function()
 		y = Math.floor(id / this.cellWidth) * this.cellSide;
 		//console.log("->" + x);
 		return new dvCell(id, x, y, this);
-	}
+	};
 	
 	this.getCellByPosition = function(x, y)
 	{	  
@@ -259,7 +258,7 @@ var dvMap = function()
 		id = (row * this.cellWidth) + column;
 		
 		return new dvCell(id, column * this.cellSide, row * this.cellSide, this);
-	}
+	};
 	
 	this.getCellByRowCol = function(row, column)
 	{
@@ -271,7 +270,7 @@ var dvMap = function()
 		id = (row * this.cellWidth) + column;
 		
 	  return new dvCell(id, column * this.cellSide, row * this.cellSide, this);
-	}
+	};
 	
 	this.draw = function()
 	{
@@ -281,11 +280,11 @@ var dvMap = function()
 		{
 			this.getCellById(i).draw();
 		}
-	}
+	};
 	
 	this.selectStartCell = function(id)
 	{
-		if (this.startCell != null)
+		if (this.startCell !== null)
 		{
 			this.unselect(this.startCell);
 		}
@@ -294,11 +293,11 @@ var dvMap = function()
 		
 		this.startCell = this.getCellById(id);		
 		this.startCell.draw();
-	}
+	};
 	
 	this.selectEndCell = function(id)
 	{
-		if (this.endCell != null)
+		if (this.endCell !== null)
 		{
 			this.unselect(this.endCell);
 		}
@@ -307,12 +306,12 @@ var dvMap = function()
 		
 		this.endCell = this.getCellById(id);		
 		this.endCell.draw();
-	}
+	};
 	
 	this.selectObstacleCell = function(id)
 	{
 		this.addObstacle(aCell);
-	}
+	};
 	
 	this.appendTo = function(container)
 	{
@@ -326,7 +325,7 @@ var dvMap = function()
 		resolveButton.on('click', function(event)
 		{
 			//Si ya construi un camino
-			if (path != null){
+			if (path !== null){
 				for (i=0;i<path.length-1;i++){
 					if (path[i].row < path[i+1].row)
 						newArray.push(2);
@@ -355,11 +354,11 @@ var dvMap = function()
 
 		resolveButton.appendTo(container);
 
-	}
+	};
 
 	this.canvas.on('click', function(event){
             
-            if(event.button == 2){ //si es el click derecho
+            if(event.button === 2){ //si es el click derecho
                 aCell = aMap.getCellByPosition(event.offsetX, event.offsetY);
                 aMap.borrarCelda(aCell.id);
             }else{
@@ -403,7 +402,7 @@ var dvMap = function()
 	
 	jQuery('body').keydown(function(event)
 	{	  
-	  if (event.keyCode == 83)
+	  if (event.keyCode === 83)
 	  {
 		result = aMap.resolve();
 	  }
