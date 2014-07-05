67bca35a30611dee7271b03f1da47dd43a2ceb2b
Rodsevich <neton_12@hotmail.com>
2014-07-05 15:53:21 -0300

    Revert "update dv.cell-0.1.0.js"
    This reverts commit 509db72d474ca495c3d74784c80e05fdfaf9b95b.

--- dv.cell-0.1.0.js
+++ dv.cell-0.1.0.js
@@ -340,8 +340,7 @@ var dvMap = function()
         var newArray = new Array();
         resolveButton.on('click', function(event)
         {
-            //Si ya construi un camino igual falta el ultimo cuadrado, 
-            // se puede poner el mismo case abajo del for pero queda medio medio
+            //Si ya construi un camino
             if (path != null) {
                 for (i = 0; i < path.length - 1; i++) {
                     if (path[i].row < path[i + 1].row)
@@ -371,20 +370,6 @@ var dvMap = function()
 
         resolveButton.appendTo(container);
 
-        // Boton de reiniciar el mapa
-        var button=document.createElement("button");
-		var t=document.createTextNode("Reiniciar mapa");
-		button.addEventListener("click", function() {
-			aMap.draw();
-			path = null;
-			queue = new Array();
-			visited = new Array();
-			queuedBy = new Array();
-			pathJSON = new Array();
-			newArray = new Array();
-		}, false);
-		button.appendChild(t);
-		document.body.appendChild(button);
     };
 
     this.canvas.on('click', function(event) {
